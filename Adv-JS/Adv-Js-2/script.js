78137f88256d5cc3db83b0bab0a2111f8bff3630
Pranab Mandal <pranavmandal5372@gmail.com>
2026-03-13 14:03:08 +0530

    Add files via upload

---
+++ Adv-JS/Adv-Js-2/script.js
@@ -0,0 +1,17 @@
+class Can {
+  constructor() {
+    this.name = "Monster Energy";
+    this.color = "crimson";
+    this.material = "Aluminium";
+    this.price = 158;
+    this.someFn = function () {};
+  }
+
+  fill(){};
+  drink(){};
+  reuse(){};
+
+}
+
+let bottle1 = new Can();
+let bottle2 = new Can();
