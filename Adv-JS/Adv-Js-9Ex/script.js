c4a6e5cd7bbe9847e0fca5041f842f6389d914cb
Pranab Mandal <pranavmandal5372@gmail.com>
2026-03-23 16:31:30 +0530

    Add files via upload

--- Adv-JS/Adv-Js-9Ex/script.js
+++ Adv-JS/Adv-Js-9Ex/script.js
@@ -11,33 +11,64 @@
 // }
 // );
 
+// Q2 ek funciton banao getUser
+// function getUser(username, cb){
+//     console.log("Fetching Username...")
+//    setTimeout(() => {
+//     const user={
+//         id:1,
+//         username: "Pranab"
+//     };
+//     cb(user);
+//    }, 3000);
+// }
 
-// Q2 ek funciton banao getUser 
-function getUser(username, cb){
-    console.log("Fetching Username...")
-   setTimeout(() => {
-    const user={
-        id:1,
-        username: "Pranab"
-    };
+// function getUserPosts(userId, cb){
+//     console.log("Loading posts...")
+//     setTimeout(() => {
+//         const posts= ["img 1", "img 2", "img 3"];
+//         console.log(" ✅ Loaded successfully. ")
+//         cb(posts)
+//     }, 4000);
+// }
+
+// getUser("Pranab",(user)=>{
+
+//     console.log("username:", user.username);
+
+//     getUserPosts(user.id,(posts)=>{
+//         console.log("Posts:",posts);
+//     })
+// })
+
+// Q3 Callback dependency - some painfull and tough question
+function loginUser(cb) {
+  setTimeout(() => {
+    const user = { id: "01", username: "Krongton" };
+    console.log("User Logged in : ", user);
     cb(user);
-   }, 3000);
+  }, 1000);
 }
 
-function getUserPosts(userId, cb){
-    console.log("Loading posts...")
-    setTimeout(() => {
-        const posts= ["img 1", "img 2", "img 3"];
-        console.log(" ✅ Loaded successfully. ")
-        cb(posts)
-    }, 4000);
+function fetchPermissions(userId, cb) {
+  setTimeout(() => {
+    const permissions = ["read", "write", "delete"];
+    console.log("Permissions for fetching Id : ", userId);
+    cb(permissions);
+  }, 1000);
 }
 
-getUser("Pranab",(user)=>{
-    
-    console.log("username:", user.username);
-    
-    getUserPosts(user.id,(posts)=>{
-        console.log("Posts:",posts);
-    })
-})
+function loadDashboard(permissions, cb) {
+  setTimeout(() => {
+    console.log("Dashboard loading with permissions...", permissions);
+    cb("✅ Dashboard Loaded.");
+  }, 1000);
+}
+
+loginUser(function (user) {
+  fetchPermissions(user.id, function (permissions) {
+    loadDashboard(permissions, function (message) {
+      console.log(message); // Final Output
+    });
+  });
+});
