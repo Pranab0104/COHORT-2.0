768047bfaa438afe99067a6c7a9d0458509eef1b
Pranab Mandal <pranavmandal5372@gmail.com>
2026-03-28 15:49:28 +0530

    Add files via upload

--- Adv-JS/Adv-Js-10Ex/script.js
+++ Adv-JS/Adv-Js-10Ex/script.js
@@ -1,15 +1,33 @@
-const prm = new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        resolve();
-    }, 3000);
-});
+// let prm = new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         resolve();
+//     }, 3000);
+// });
 
-prm.then(function(){
-    console.log("Program resolve...");
+// prm.then(function(){
+//     console.log("Program resolve...");
     
+// })
+
+// .catch(function(){
+//     console.log("Program faced some issued...");
+    
+// })
+
+fetch('https://randomuser.me/api/')
+.then(function(notreadable){
+    return notreadable.json();
 })
 
-.catch(function(){
-    console.log("Program faced some issued...");
-    
+.then(function(realData){
+    console.log(realData.results[0].name.first);
 })
+
+//async & await example
+async function abc() { 
+    let raw = await fetch('https://randomuser.me/api/');
+    let data = await raw.json();
+    console.log(data);
+}
+
+abc();
