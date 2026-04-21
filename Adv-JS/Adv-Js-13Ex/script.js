2a856d939fb5c718893d2247cf9874c74a163614
Pranab Mandal <pranavmandal5372@gmail.com>
2026-04-21 22:44:31 +0530

    Add files via upload

---
+++ Adv-JS/Adv-Js-13Ex/script.js
@@ -0,0 +1,19 @@
+const users = [
+    "sonu@gmail.com",
+    "bheem@gmail.com",
+    "kaila@gmail.com",
+    "karn@gmail.com",
+
+];
+
+function sendEmail(email){
+    return new Promise((resolve, reject)=>{
+        
+        let time =Math.floor(Math.random()*6)
+
+        setTimeout(() => {
+            
+            
+        }, time * 1000);
+    })
+}
