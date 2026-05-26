ec430377acdf7efa00ed307a614a467993f619bf
Pranab Mandal <pranavmandal5372@gmail.com>
2026-05-26 23:13:47 +0530

    Add files via upload

--- Projects/DashBoard-Project/script.js
+++ Projects/DashBoard-Project/script.js
@@ -15,5 +15,62 @@ function frontpageTrans() {
   });
 }
 
-frontpageTrans();
+// frontpageTrans();
 
+let form = document.querySelector(".addTask form");
+let Taskinput = document.querySelector(".addTask form #tk");
+let Descinput = document.querySelector(".addTask form textarea");
+let Check = document.querySelector(".addTask form #check");
+
+let Tak = [
+  {
+    task: "Wake Up Early before sunrise",
+    details: "To maintain the time or enery in a better way.",
+    imp: true,
+  },
+
+  {
+    task: "Do Exercise / Go to Gym on daily basis",
+    details: "To maintain the body or health.",
+    imp: true,
+  },
+
+  {
+    task: "Work on your skills for a best life",
+    details: "Invest on your most time here which will pay you later.",
+    imp: true,
+  },
+
+  {
+    task: "Use handset / Mobile",
+    details: "Only for good things not for the entertainment.",
+    imp: false,
+  },
+];
+
+function Todopage() {
+  let tk = document.querySelector(".allTask");
+  let sum = "";
+  // console.log(tk);
+  Tak.forEach(function (elem) {
+    sum += `<div class="task">
+                <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+                <button>Accomplished</button>
+              </div>`;
+  });
+
+  tk.innerHTML = sum;
+}
+
+Todopage();
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // console.log(Taskinput.value);
+  // console.log(Descinput.value);
+  // console.log(Check.checked);
+
+  Tak.push({task:tak.value, details: taskdetailsinput.value, imp:impCheckbox.checked})
+  console.log(Tak);
+  
+});
