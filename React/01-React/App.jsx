d0817b5f4ac93db188b1554d313d3cc057efff60
Pranab Mandal <pranavmandal5372@gmail.com>
2026-07-21 23:25:40 +0530

    Add files via upload

---
+++ React/01-React/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     {/* <h1> Helloooo Master!</h1><hr />
+     <h2>I am a student of Cohort 2.0 </h2> */}
+    </div>
+  )
+}
+
+export default App
---
+++ React/01-React/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Nav from "./assets/Nav.jsx";
+import Home from "./assets/Home.jsx";
+import Menu from "./assets/Menu.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <App />
+    <Nav />
+    <Home/>
+    <Menu/>
+  </div>,
+);
