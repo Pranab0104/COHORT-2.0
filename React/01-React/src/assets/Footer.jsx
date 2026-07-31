26b52109cc42191cb92390a39fc34c178dcd9414
Pranab Mandal <pranavmandal5372@gmail.com>
2026-07-31 23:00:16 +0530

    Add files via upload

---
+++ React/01-React/src/assets/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="Foot">
+      <h1>
+        Bringing warmth, freshness, and handcrafted bakery delights to every
+        table.
+      </h1>
+
+      <p>
+        " Every memorable celebration begins with a conversation. Whether you're
+        ordering handcrafted pastries, planning a custom cake, or simply have a
+        question, we'd love to hear from you. Share your ideas, and let's bake
+        something unforgettable together. "
+      </p>
+      <div className="conta">
+        <div className="Card">
+          <div className="Baxxaa"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
