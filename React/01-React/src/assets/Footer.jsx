b61d1dc9bab9610309fb5cc7e790f78a3b887f27
Pranab Mandal <pranavmandal5372@gmail.com>
2026-08-02 23:52:35 +0530

    Add files via upload

--- React/01-React/src/assets/Footer.jsx
+++ React/01-React/src/assets/Footer.jsx
@@ -16,7 +16,26 @@ const Footer = () => {
       </p>
       <div className="conta">
         <div className="Card">
-          <div className="Baxxaa"></div>
+          <div className="Baxxa">
+            <div className="one">
+              <h4>Doughvia</h4>
+              <h5>Where Every Bite Feels Like Home.</h5>
+            </div>
+            <div className="one">
+              <h4>Quick Links</h4>
+              <h5>Home</h5>
+              <h5>About Us</h5>
+              <h5>Menu</h5>
+              <h5>Moments</h5>
+              <h5>Contact Us</h5>
+            </div>
+            <div className="one">
+              <h4>Contact</h4>
+              <h5></h5>
+              <h5></h5>
+              <h5></h5>
+            </div>
+          </div>
         </div>
       </div>
     </div>
