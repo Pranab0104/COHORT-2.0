fc33795521e3b94e14aec227f436cb1537a2d237
Pranab Mandal <pranavmandal5372@gmail.com>
2026-09-10 22:18:09 +0530

    Add files via upload

--- React/02-React/src/pages/Birds.jsx
+++ React/02-React/src/pages/Birds.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { LiaLongArrowAltDownSolid } from "react-icons/lia";
 
 const Birds = () => {
@@ -19,25 +20,33 @@ const Birds = () => {
           <span>01</span>
           <h4>RAPTORS</h4>
           <p>Hunters of the open sky.</p>
-          <i>↗</i>
+          <Link to="/raptors">
+            <i>↗</i>
+          </Link>
         </div>
         <div id="box2" className="boxee ">
           <span>02</span>
           <h4>SONGBIRDS</h4>
           <p>Voices of the wild sky.</p>
-          <i>↗</i>
+          <Link to="/songbirds">
+            <i>↗</i>
+          </Link>
         </div>
         <div id="box3" className="boxee ">
           <span>03</span>
           <h4>WATERFOWL</h4>
           <p>Life between land and water.</p>
-          <i>↗</i>
+          <Link to="/waterfowl">
+            <i>↗</i>
+          </Link>
         </div>
         <div id="box4" className="boxee ">
           <span>04</span>
           <h4>EXOTIC BIRDS</h4>
           <p>Color beyond imagination.</p>
-          <i>↗</i>
+          <Link to="/exoticbirds">
+            <i>↗</i>
+          </Link>
         </div>
       </div>
 
